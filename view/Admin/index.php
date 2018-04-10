e142991d5e2a7d81dd82b30f7f76804fb0080179
yo5bdm <yo5bdm@gmail.com>
2018-04-10 15:03:13 +0300

    bun

--- view/Admin/index.php
+++ view/Admin/index.php
@@ -15,8 +15,27 @@
                 </ul>
                 <hr/>
                 <p>Adaugă o categorie nouă:</p>
-                <input class="form-control" ng-model="form.denumire" placeholder="Denumire categorie"/> <br/>
+                <input class="form-control" ng-model="form.denumireCat" placeholder="Denumire categorie"/> <br/>
                 <button ng-click="adaugaCategorie()" class="form-control btn btn-success">Adaugă categoria</button>
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-heading"><h4>Baze de date:</h4></div>
+            <div class="panel-body">
+                <ul class="list-group">
+                    <li class="list-group-item" ng-repeat="x in json.BazeDeDate">
+                        {{x.denumire}}
+                        <span class="glyphicon glyphicon-remove pull-right" title="Sterge selectia" ng-click="stergeDB(x.id)"></span>                        
+                    </li>
+                    <li class="list-group-item list-group-item-info" ng-hide="json.BazeDeDate.length">Nu sunt baze de date de afisat</li>
+                </ul>
+                <hr/>
+                <p>Adaugă o bază de date nouă:</p>
+                <input class="form-control" ng-model="form.denumireDB" placeholder="Denumire bază de date"/> <br/>
+                <button ng-click="adaugaDB()" class="form-control btn btn-success">Adaugă categoria</button>
             </div>
         </div>
     </div>
@@ -32,11 +51,12 @@ app.controller("myCtrl", ['$scope','$http', function($scope,$http) {
     });
     //metode
     promise.then(function(data){
-        $scope.form.denumire='';
+        $scope.form.denumireCat='';
+        $scope.form.denumireDB ='';
         $scope.adaugaCategorie = function() {
             var cat = {
                 id:$scope.getNextCategorieID(),
-                denumire:$scope.form.denumire
+                denumire:$scope.form.denumireCat
             };
             $scope.json.Categorii.push(cat);
             $scope.salveazaCategorii();
@@ -61,7 +81,6 @@ app.controller("myCtrl", ['$scope','$http', function($scope,$http) {
                 }
             };
         };
-        
         
         $scope.coboara = function(ids) {
             for(var i=0;i<$scope.json.Categorii.length;i++) {
@@ -105,6 +124,47 @@ app.controller("myCtrl", ['$scope','$http', function($scope,$http) {
                 console.log(response);
             });
         };
+        
+        //baze de date
+        $scope.adaugaDB = function() {
+            var db = {
+                id:$scope.getNextDbID(),
+                denumire:$scope.form.denumireDB
+            };
+            $scope.json.BazeDeDate.push(db);
+            $scope.salveazaDB();
+        };
+        $scope.getNextDbId = function() {
+            var id = 0;
+            $scope.json.BazeDeDate.filter(function(cat){
+                if(cat.id>id) id = cat.id;
+                return false;
+            });
+            return id+1;
+        };
+        $scope.stergeDB = function() {
+            for(var i=0;i<$scope.json.BazeDeDate.length;i++) {
+                if($scope.json.BazeDeDate[i].id == ids) {
+                    $scope.json.BazeDeDate.splice(i,1);
+                    $scope.salveazaDB();
+                    break;
+                }
+            }
+        };
+        $scope.salveazaDB = function() {
+            var param = {
+            datele:$scope.json.BazeDeDate
+            };
+            var config = {
+                headers : {
+                    'Content-Type': 'application/json'
+                }
+            };
+            $http.post("<?= Helpers::generateUrl(['c'=>'json','a'=>'savebazededate'])?>",param,config).then(function(response){
+                console.log(response);
+            });
+        };
+        
     });
 }]);
 </script>
